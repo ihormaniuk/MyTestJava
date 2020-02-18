166c64ca6861e2cf67fd4b373dfb4d369bba52d5
ihormaniuk <ihor.maniuk@gmail.com>
2020-02-18 16:33:08 +0200

    Update Gerbil.java

--- src/ecel/Gerbil.java
+++ src/ecel/Gerbil.java
@@ -1,11 +1,14 @@
 package ecel;
 
+import java.util.Random;
+
 public class Gerbil {
     int gerbilNumber ;
+    Random random = new Random();
     public Gerbil(int gerbilNumber){
         this.gerbilNumber=gerbilNumber;
     }
-    public String hop(){
+    public String hop(int i){
         String messenge = "Hi this is Method hop : ";
         return messenge + gerbilNumber;
     }
