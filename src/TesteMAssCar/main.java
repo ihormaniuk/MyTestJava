339d2ddece6fb7bc5a872f8382cc86ef8d61ea50
ihormaniuk <ihor.maniuk@gmail.com>
2020-02-21 18:19:45 +0200

     master

--- src/TesteMAssCar/main.java
+++ src/TesteMAssCar/main.java
@@ -9,38 +9,45 @@ import static PrintAndScanner.Print.print;
 public class main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        print("");
+        print("1 : Вивести toString() даних елементів масиву на консоль. ( deepToString() )");
+        print("2 : відсортувати за спаданням всі обєкти за введеним полем ");
+        print("3 : відсортувати за зростанням  всі обєкти за введеним полем ");
+
+        Auto auto[][] = new Auto[randomGeneret(1, 5)][randomGeneret(1, 5)];
+
+        for (int i = 0; i < auto.length; i++) {
+            for (int j = 0; j < auto[i].length; j++) {
+                auto[i][j] = new Auto(randomGeneret(100, 250),
+                        randomGeneret(1980, 2018),
+                        new Engine(randomGeneret(2, 12)),
+                        new Helm(randomGeneret(35,44),"Alcantara"));
+            }
+        }
+
         switch (scanner.nextInt()) {
 
             case 1: {
 
-                Auto autoArray[][] = new Auto[getRandomValueFromRange(1, 5)][getRandomValueFromRange(1, 5)];
+//                Auto autoArray[][] = new Auto[getRandomValueFromRange(1, 5)][getRandomValueFromRange(1, 5)];
+//
+//                for (int i = 0; i < autoArray.length; i++) {
+//                    for (int j = 0; j < autoArray[i].length; j++) {
+//                        autoArray[i][j] = new Auto(getRandomValueFromRange(100, 250),
+//                                                        getRandomValueFromRange(1980, 2018),
+//                                new Engine(getRandomValueFromRange(2, 12)),
+//                                new Helm(getRandomValueFromRange(35,44),"kks"));
+//                    }
+//                }
 
-                for (int i = 0; i < autoArray.length; i++) {
-                    for (int j = 0; j < autoArray[i].length; j++) {
-                        autoArray[i][j] = new Auto(getRandomValueFromRange(100, 250),
-                                                        getRandomValueFromRange(1980, 2018),
-                                new Engine(getRandomValueFromRange(2, 12)),
-                                new Helm(getRandomValueFromRange(35,44),"kks"));
-                    }
-                }
-
-                System.out.println(Arrays.deepToString(autoArray));
+                System.out.println(Arrays.deepToString(auto));
 
                 break;
             }
 
             case 2: {
-
-                Auto auto = new Auto(getRandomValueFromRange(100, 250), getRandomValueFromRange(1980, 2018),
-                        new Engine(getRandomValueFromRange(2, 12)),
-                        new Helm(getRandomValueFromRange(35,44),"kks"));
-
-                Auto autoArray[] = new Auto[getRandomValueFromRange(1, 5)];
-
-                Arrays.fill(autoArray, auto);
-
-                System.out.println(Arrays.deepToString(autoArray));
+                Auto auto[][] = new Auto[randomGeneret(1, 5)][randomGeneret(1, 5)];
+                Arrays.sort(auto);
+                print(Arrays.toString(auto));
 
                 break;
             }
@@ -53,13 +60,13 @@ public class main {
 
         }
     }
-    static int getRandomValueFromRange(int min, int max) {
+    static int randomGeneret(int min, int max) {
 
         if (min >= max) {
             throw new IllegalArgumentException("Min value must be less than max value!");
         }
 
-        Random randomValue = new Random();
-        return randomValue.nextInt(max - min + 1) + min;
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
 }
