b2dfdb611ac6356d17329c36be8f6858f31c7574
ihormaniuk <ihor.maniuk@gmail.com>
2020-03-01 14:38:11 +0200

    master

--- src/convertChartoNumber.java
+++ src/convertChartoNumber.java
@@ -2,14 +2,17 @@ import static PrintAndScanner.Print.print;
 
 public class convertChartoNumber {
     public static void main(String[] args){
-        String s = "Привіт Я Ігор Мені 24";
-        char[] chars = s.toCharArray();
-        for(int i = 0; i < chars.length;i++){
-            print(chars[i]);
-            print((int) s.charAt(i));
-        }
-        char c = 1055;
+        double a = 10.0,b = 20.0;
+        double c = Math.sqrt(a*a+b*b);
         print(c);
+//        String s = "Привіт Я Ігор Мені 24";
+//        char[] chars = s.toCharArray();
+//        for(int i = 0; i < chars.length;i++){
+//            print(chars[i]);
+//            print((int) s.charAt(i));
+//        }
+//        char c = '\ua432';
+//        print(c);
 
 //        for(int a = 0;a<chars.length;a++){
 //            int b = Character.getNumericValue(chars[a]);
