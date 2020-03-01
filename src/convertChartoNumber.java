a0d2ac7151ff4478cac001a65abdb49e52cd6b28
ihormaniuk <ihor.maniuk@gmail.com>
2020-03-01 13:45:19 +0200

    LastTeste

--- src/convertChartoNumber.java
+++ src/convertChartoNumber.java
@@ -1,2 +1,19 @@
+import static PrintAndScanner.Print.print;
+
 public class convertChartoNumber {
+    public static void main(String[] args){
+        String s = "Привіт Я Ігор Мені 24";
+        char[] chars = s.toCharArray();
+        for(int i = 0; i < chars.length;i++){
+            print(chars[i]);
+            print((int) s.charAt(i));
+        }
+        char c = 1055;
+        print(c);
+
+//        for(int a = 0;a<chars.length;a++){
+//            int b = Character.getNumericValue(chars[a]);
+//            print(b);
+//        }
+    }
 }
--- src/scann.java
+++ src/scann.java
@@ -10,10 +10,18 @@ public class scann {
         String filet = seperator + "C:" + seperator + "test.txt";
         File file = new File ( filet );
         Scanner scanner = new Scanner ( file);
+
         while (scanner.hasNextLine ()) {
             List<String> st = new ArrayList<> ( Collections.singleton ( scanner.nextLine () ) );
 //            Arrays.sort ( new String[]{scanner.nextLine ()} );
             print ( st  );
+            String s = scanner.toString();
+            char[] chars = s.toCharArray();
+            for(int i = 0; i < chars.length;i++){
+                print(chars[i]);
+                print((int) s.charAt(i));
+            }
+
 //            scanner.close ();
         }
 
